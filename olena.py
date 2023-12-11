88752015bb386e39f79111b14200d91e53dd5629
JuliaVahula2 <136905242+JuliaVahula2@users.noreply.github.com>
2023-12-11 17:03:48 +0200

    add file with program

---
+++ olena.py
@@ -0,0 +1,19 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import*
+
+app=QApplication([])
+win=QWidget()
+
+win.resize(500,500)
+win.setWindowTitle('Тест ГітХаб')
+
+title=QLabel('Тестовий додаток для ГітХаб')
+
+line_h=QHBoxLayout()
+line_h.addChildWidget(title)
+
+win.setLayout(line_h)
+
+win.show()
+win.show(lene_h)
+app.exec_()
